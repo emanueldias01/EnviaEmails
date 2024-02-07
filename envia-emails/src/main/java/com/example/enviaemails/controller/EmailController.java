d498131de9f04430aafbc8e43d0e16b82becdc80
Your Name <emanuelaraujo90@gmail.com>
2024-02-07 09:02:24 -0300

    Implementação do service

--- envia-emails/src/main/java/com/example/enviaemails/controller/EmailController.java
+++ envia-emails/src/main/java/com/example/enviaemails/controller/EmailController.java
@@ -1,4 +1,24 @@
 package com.example.enviaemails.controller;
 
+import com.example.enviaemails.entity.Email;
+import com.example.enviaemails.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("envia-email")
 public class EmailController {
+
+    @Autowired
+    EmailService service;
+
+    @PostMapping
+    public void sendEmail(@RequestBody Email email){
+        service.enviaEmail(email);
+
+    }
+
 }
