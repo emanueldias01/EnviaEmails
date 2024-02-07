a2a3a35206aba67b5f76fea071a75d2dba2ca0ba
Your Name <emanuelaraujo90@gmail.com>
2024-02-07 09:02:55 -0300

    Anota a classe Service para identificação do Spring

--- envia-emails/src/main/java/com/example/enviaemails/service/EmailService.java
+++ envia-emails/src/main/java/com/example/enviaemails/service/EmailService.java
@@ -4,7 +4,10 @@ import com.example.enviaemails.entity.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class EmailService {
 
     @Autowired
