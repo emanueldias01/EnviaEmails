e54854a454e1cc2b25fa014167b2e9e8959bc919
Your Name <emanuelaraujo90@gmail.com>
2024-02-07 08:59:41 -0300

    implementa lógica de enviar email no service

--- envia-emails/src/main/java/com/example/enviaemails/service/EmailService.java
+++ envia-emails/src/main/java/com/example/enviaemails/service/EmailService.java
@@ -1,4 +1,23 @@
 package com.example.enviaemails.service;
 
+import com.example.enviaemails.entity.Email;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class EmailService {
+
+    @Autowired
+    JavaMailSender mailSender;
+
+    public void enviaEmail(Email email){
+
+        var mensagem = new SimpleMailMessage();
+        mensagem.setTo(email.destino());
+        mensagem.setSubject(email.assunto());
+        mensagem.setText(email.escrito());
+
+        mailSender.send(mensagem);
+    }
+
 }
