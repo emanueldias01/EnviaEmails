e121ae87832babf708fc1b569138f91eaf1ee0f7
Your Name <emanuelaraujo90@gmail.com>
2024-02-07 08:51:39 -0300

    upar arquivos

---
+++ envia-emails/src/main/java/com/example/enviaemails/EnviaEmailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.enviaemails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviaEmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviaEmailsApplication.class, args);
+	}
+
+}
---
+++ envia-emails/src/test/java/com/example/enviaemails/EnviaEmailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enviaemails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviaEmailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
